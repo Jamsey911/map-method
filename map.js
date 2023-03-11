3c43700f53b038d7b7fd62d6790a2a9f0859896f
Jamsey911 <jamesalynch911@hotmail.com>
2023-03-11 16:47:09 +0000

    Inital commit

--- map.js
+++ map.js
@@ -5,17 +5,26 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums){
+  results.push (num * 2);
+};
+console.log(results);
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+};
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
-
+const simple = nums.map(function (num) {return num * 2});
+console.log(simple);
 // Simplfied w/ map() + arrow function
-
-
+const simpleArrow = nums.map(num => num * 2);
+console.log(simpleArrow);
 // With objects:
 const students = [
   {
@@ -37,3 +46,9 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.id, student.name]);
+console.log(studentsWithIds)
+
+const studentsNameAndId = students.map(student => [student.id, student.name]);
+console.log(studentsNameAndId)
